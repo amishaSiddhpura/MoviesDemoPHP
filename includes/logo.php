b13f349d3062ad4cdad081a602bc98bf4c35f311
Amisha <Amisha@DESKTOP-MJFGCFR>
2020-07-10 17:14:14 -0400

    push

---
+++ includes/logo.php
@@ -0,0 +1,8 @@
+<div id="titlemenu">
+<table border=0><tr>
+	<td><div align="left"><span class ="titletext"> <font color = "#CFB53B"><?php echo $logoTitle."<br>"; ?></font></span></div></td>
+	<td><div id= "imagecontent">
+	<img src= <?php echo $logoImage; ?> height="auto" width="150"></div></td>
+	</tr></table>
+</div>
+
