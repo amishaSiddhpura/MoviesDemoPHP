17a8fd6e0e7d6b75509ac4abf0d94038e7b45f37
Amisha <Amisha@DESKTOP-MJFGCFR>
2020-07-10 17:28:59 -0400

    push

--- classes/DB_Config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$HOST = 'localhost';
-$USER = 'root';
-$PASSWORD = '';
-$NAME = 'movies_db';
-
-
-?>
--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="footer" class="smallgraytext">
-<a href="#">Home</a> | <a href="#">About Us</a> | <a href="#">Products</a> | <a href="#">Contact Us</a>
-</div>
